23f54c390439427d093c27c46378368c060fd9bc
Keiver Pacheco <keiver.pacheco@dis-global.com>
2024-09-10 11:17:28 -0400

    first init

---
+++ src/layouts/components/workspaces-popover.tsx
@@ -0,0 +1,15 @@
+import Box from '@mui/material/Box';
+import { Typography } from '@mui/material';
+
+export function WorkspacesPopover() {
+ 
+  return (
+    <Box display="flex" alignItems="center" width="20%" height="20%" justifyContent="center">
+      <Box ml={1}>
+        <Typography variant="body2" fontWeight="fontWeightSemiBold">
+          Cacica
+        </Typography>
+      </Box>
+    </Box>
+  );
+}
